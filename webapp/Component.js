f25fa0b32c1f2e5cf70953bac2719c1e355709bb
Singh Vaibhav <vsingh@mysupplychaingroup.com>
2018-11-02 08:27:13 +0000

    Webide Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,22 @@
+(function () {
+	"use strict";
+	/*global sap, jQuery */
+
+	/**
+	 * @fileOverview Application component to display information on entities from the GWSAMPLE_BASIC
+	 *   OData service.
+	 * @version @version@
+	 */
+	jQuery.sap.declare("com.ui.ewmodoovpapp.Component");
+
+	jQuery.sap.require("sap.ovp.app.Component");
+
+	sap.ovp.app.Component.extend("com.ui.ewmodoovpapp.Component", {
+		metadata: {
+			manifest: "json"
+		},
+		init: function() {
+			sap.ovp.app.Component.prototype.init.apply(this, arguments);
+		}
+	});
+}());
